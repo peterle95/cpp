8cef422698ab0cc241b93d3f4d9bc417b885733c
Peter Moelzer <moelzerpeter@gmail.com>
2025-03-12 14:27:36 +0100

    norm

---
+++ cpp03/ex02/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 16:23:56 by pmolzer           #+#    #+#             */
+/*   Updated: 2025/03/12 14:27:28 by pmolzer          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap 
+{
+protected:
+    std::string name;
+    unsigned int hitPoints;
+    unsigned int energyPoints;
+    unsigned int attackDamage;
+
+public:
+    ClapTrap(std::string name);
+    ~ClapTrap();
+
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+#endif
