c5ec790a158708157e1a4f71b89d1224c10c8911
Peter Moelzer <moelzerpeter@gmail.com>
2025-03-20 14:45:31 +0100

    norm

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 16:41:58 by pmolzer           #+#    #+#             */
-/*   Updated: 2024/11/11 20:58:50 by pmolzer          ###   ########.fr       */
+/*   Updated: 2025/03/20 14:09:18 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,30 @@
 #include "Cat.hpp"
 #include <iostream>
 
-/*TASK OF THE PROGRAM*/
+/*TASK OF THE PROGRAM
+Constructors and destructors of each class must display specific messages.
+Implement a Brain class. It contains an array of 100 std::string called ideas.
+This way, Dog and Cat will have a private Brain* attribute.
+Upon construction, Dog and Cat will create their Brain using new Brain();
+Upon destruction, Dog and Cat will delete their Brain.
+In your main function, create and fill an array of Animal objects. Half of it will
+be Dog objects and the other half will be Cat objects. At the end of your program
+execution, loop over this array and delete every Animal. You must delete directly dogs
+and cats as Animals. The appropriate destructors must be called in the expected order.
+Don’t forget to check for memory leaks.
+A copy of a Dog or a Cat mustn’t be shallow. Thus, you have to test that your copies
+are deep copies!
+
+int main()
+{
+const Animal* j = new Dog();
+const Animal* i = new Cat();
+delete j;//should not create a leak
+delete i;
+...
+return 0;
+}
+Implement and turn in more tests than the ones given above.*/
 
 int main()
 {
@@ -26,12 +49,9 @@ int main()
     for (int i = 0; i < arraySize; ++i) 
     {
         if (i % 2 == 0) 
-        {
             animals[i] = new Dog();
-        } else 
-        {
+        else
             animals[i] = new Cat();
-        }
     }
 
     // Test deep copy
@@ -49,9 +69,7 @@ int main()
 
     // Delete all animals in the array
     for (int i = 0; i < arraySize; ++i) 
-    {
         delete animals[i];
-    }
 
     return 0;
 }
