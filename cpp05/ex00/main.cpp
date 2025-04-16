a514c0631082d3350178c2ad2841d36108b7d5ef
Peter Moelzer <moelzerpeter@gmail.com>
2025-04-16 16:47:29 +0200

    norm

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 13:33:05 by pmolzer           #+#    #+#             */
-/*   Updated: 2025/04/16 16:22:33 by pmolzer          ###   ########.fr       */
+/*   Updated: 2025/04/16 16:44:27 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,43 @@
 
 int main() 
 {
+    
+    std::string word = "four";
+
+    try
+    {
+        std::cout << word.at(4) << std::endl; // this will be an out of range error
+    }
+    /* catch(...) // exeptions are thrown with a type, like (...) --> catches any type of exeption
+    {
+        // std::cerr << e.what() << '\n';
+        std::cout << "Exeption thrown!" << std::endl;
+    } */
+    catch(std::out_of_range& e) 
+    /* when the exeption is thrown, an out_of_range type object is going to be previded. 
+    "e" is the reference to that object. e is an object that has a method called "what".
+    Any "what" method is going to return a string which may contain information about the exeption that is taking place*/
+    {
+        // std::cerr << e.what() << '\n';
+        std::cout << "Exeption:" << e.what() << std::endl;
+    }
+
+    /* try
+    {
+        int *array = new int[99999999999999999999999999];
+    }
+    catch(std::out_of_range& e) // this is not going to catch it because it's another type of exeption
+    {
+        std::cout << "Exeption:" << e.what() << std::endl;
+    }
+    catch(std::bad_alloc& e)
+    {
+        std::cout << "Second Exeption:" << e.what() << std::endl;
+    } */
+
+   // --> Exeption types have a hierarchy "exception" is the highest. LOOK THIS UP
+    
+    
    // FIRST TEST CASE: Testing incrementGrade() when reaching grade limit
    try 
    {
