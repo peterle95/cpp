b385ca2101a28bb84b9e2cb96e164a0214f2ea24
Peter Moelzer <moelzerpeter@gmail.com>
2025-04-17 15:25:13 +0200

    norm

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -6,69 +6,82 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 11:21:31 by pmolzer           #+#    #+#             */
-/*   Updated: 2025/04/11 11:21:32 by pmolzer          ###   ########.fr       */
+/*   Updated: 2025/04/17 15:20:54 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
+// Default constructor initializes the bureaucrat with a default name and grade.
 Bureaucrat::Bureaucrat() : _name("default"), _grade(150) {
 }
 
+// Parameterized constructor initializes the bureaucrat with a given name and grade.
+// Throws exceptions if the grade is out of valid range (1-150).
 Bureaucrat::Bureaucrat(const std::string& name, int grade) : _name(name) {
     if (grade < 1) {
-        throw GradeTooHighException();
+        throw GradeTooHighException(); // Grade cannot be higher than 1.
     }
     if (grade > 150) {
-        throw GradeTooLowException();
+        throw GradeTooLowException(); // Grade cannot be lower than 150.
     }
-    this->_grade = grade;
+    this->_grade = grade; // Set the grade if it's valid.
 }
 
+// Copy constructor for creating a Bureaucrat from another Bureaucrat.
 Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name) {
-    *this = other;
+    *this = other; // Use the assignment operator to copy the grade.
 }
 
+// Destructor for Bureaucrat (no dynamic memory to free).
 Bureaucrat::~Bureaucrat() {}
 
+// Assignment operator to copy the grade from another Bureaucrat.
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
     if (this != &other) {
-        this->_grade = other._grade;
+        this->_grade = other._grade; // Copy the grade.
     }
-    return *this;
+    return *this; // Return the current object.
 }
 
+// Getter for the bureaucrat's name.
 const std::string& Bureaucrat::getName() const {
-    return this->_name;
+    return this->_name; // Return the name.
 }
 
+// Getter for the bureaucrat's grade.
 int Bureaucrat::getGrade() const {
-    return this->_grade;
+    return this->_grade; // Return the grade.
 }
 
+// Method to increment the bureaucrat's grade (make it higher).
 void Bureaucrat::incrementGrade() {
     if (this->_grade - 1 < 1) {
-        throw GradeTooHighException();
+        throw GradeTooHighException(); // Cannot increment beyond grade 1.
     }
-    this->_grade--;
+    this->_grade--; // Decrement the grade (higher rank).
 }
 
+// Method to decrement the bureaucrat's grade (make it lower).
 void Bureaucrat::decrementGrade() {
     if (this->_grade + 1 > 150) {
-        throw GradeTooLowException();
+        throw GradeTooLowException(); // Cannot decrement beyond grade 150.
     }
-    this->_grade++;
+    this->_grade++; // Increment the grade (lower rank).
 }
 
+// Overloaded output stream operator to print Bureaucrat information.
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
     os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade();
-    return os;
+    return os; // Return the output stream.
 }
 
+// Implementation of the GradeTooHighException's what() method.
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
-    return "Grade too high";
+    return "Grade too high"; // Return error message for too high grade.
 }
 
+// Implementation of the GradeTooLowException's what() method.
 const char* Bureaucrat::GradeTooLowException::what() const throw() {
-    return "Grade too low";
+    return "Grade too low"; // Return error message for too low grade.
 }
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:02:06 by pmolzer           #+#    #+#             */
-/*   Updated: 2025/04/16 13:36:51 by pmolzer          ###   ########.fr       */
+/*   Updated: 2025/04/17 15:24:00 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,37 +17,51 @@
 #include <iostream>
 #include <exception>
 
+// The Bureaucrat class represents a bureaucrat with a name and a grade.
 class Bureaucrat
 {
 private:
-    const std::string _name;
-    int _grade;
+    const std::string _name; // The name of the bureaucrat, cannot be changed after initialization.
+    int _grade; // The grade of the bureaucrat, which determines their rank.
 
 public:
-    Bureaucrat();
-    Bureaucrat(const std::string& name, int grade);
-    Bureaucrat(const Bureaucrat& other);
-    ~Bureaucrat();
-    Bureaucrat& operator=(const Bureaucrat& other);
+    Bureaucrat(); // Default constructor.
+    Bureaucrat(const std::string& name, int grade); // Parameterized constructor.
+    Bureaucrat(const Bureaucrat& other); // Copy constructor.
+    ~Bureaucrat(); // Destructor.
+    Bureaucrat& operator=(const Bureaucrat& other); // Assignment operator.
 
+    // Nested exception class for handling cases where the grade is too high.
+    // The GradeTooHighException class is a nested exception class that 
+    // inherits from std::exception.
+    // The throw() specifier indicates that this function does not throw any exceptions.
+    // It is used to inform the compiler and the programmer that 
+    // the function is guaranteed not to throw,
+    // which can help with optimization and exception safety.
     class GradeTooHighException : public std::exception 
     {
         public: 
-            virtual const char* what() const throw();
+            virtual const char* what() const throw(); // Override what() to return an error message.
     };
 
+    // Nested exception class for handling cases where the grade is too low.
     class GradeTooLowException : public std::exception 
     {
         public: 
-            virtual const char* what() const throw();
+            virtual const char* what() const throw(); // Override what() to return an error message.
     };
 
+    // Getter for the bureaucrat's name.
     const std::string& getName() const;
+    // Getter for the bureaucrat's grade.
     int getGrade() const;
 
+    // Method to increment the bureaucrat's grade (make it higher).
     void incrementGrade();
+    // Method to decrement the bureaucrat's grade (make it lower).
     void decrementGrade();
 };
 
+// Overloaded output stream operator to print Bureaucrat information.
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat);
 #endif
