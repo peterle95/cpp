a0c8de1dd759c381e7f1bb413bb68c3467057b11
Peter Moelzer <moelzerpeter@gmail.com>
2025-05-08 14:17:04 +0200

    added colors

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 15:10:25 by pmolzer           #+#    #+#             */
-/*   Updated: 2025/04/16 14:18:47 by pmolzer          ###   ########.fr       */
+/*   Updated: 2025/05/08 14:16:33 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,23 @@ int main()
     try 
     {
         // Test basic Form functionality
-        Form taxForm("Tax Form", 50, 25);
+        Form taxForm("\033[31mTax Form\033[0m", 50, 25);
         std::cout << taxForm << std::endl;
         
         // Test high-level bureaucrat signing a form
-        Bureaucrat highLevel("Director", 10);
+        Bureaucrat highLevel("\033[92mDirector\033[0m", 10);
         std::cout << highLevel << std::endl;
         highLevel.signForm(taxForm);
         std::cout << taxForm << std::endl;
         
         // Test attempting to sign an already signed form
-        Bureaucrat anotherHighLevel("Manager", 20);
+        Bureaucrat anotherHighLevel("\033[92mManager\033[0m", 20);
         anotherHighLevel.signForm(taxForm);
         
         // Test low-level bureaucrat failing to sign a form
-        Form secretForm("Secret Clearance", 15, 5);
+        Form secretForm("\033[93mSecret Clearance\033[0m", 15, 5);
         std::cout << secretForm << std::endl;
-        Bureaucrat lowLevel("Intern", 100);
+        Bureaucrat lowLevel("\033[92mIntern\033[0m", 100);
         std::cout << lowLevel << std::endl;
         lowLevel.signForm(secretForm);
         std::cout << secretForm << std::endl;
@@ -45,7 +45,7 @@ int main()
     } 
     catch (std::exception& e) 
     {
-        std::cout << "Exception: " << e.what() << std::endl;
+        std::cout << "\033[31mException: \033[0m" << e.what() << std::endl;
     }
     
     try 
@@ -55,7 +55,7 @@ int main()
     } 
     catch (std::exception& e) 
     {
-        std::cout << "Exception: " << e.what() << std::endl;
+        std::cout << "\033[31mException: \033[0m" << e.what() << std::endl;
     }
 
     return 0;
