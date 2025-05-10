0c042253e22854f42db603f7e0d8625ac0423bdf
Peter Moelzer <moelzerpeter@gmail.com>
2025-05-10 16:07:25 +0200

    new makefile

--- cpp05/ex02/RobotomyRequestForm.cpp
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 14:14:12 by pmolzer           #+#    #+#             */
-/*   Updated: 2025/04/16 14:17:38 by pmolzer          ###   ########.fr       */
+/*   Updated: 2025/05/10 15:55:15 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,11 +47,30 @@ void RobotomyRequestForm::execute(Bureaucrat const & executor) const
 
     std::cout << "* DRILLING NOISES *" << std::endl;
     
-    // Seed the random number generator
+    // Seed the random number generator with the current time
+    // std::time(NULL) returns the current time as seconds since Jan 1, 1970
+    // Using this value ensures we get different random numbers each time the program runs
     std::srand(std::time(NULL));
+    /*Here's why srand() is necessary:
+
+    Pseudorandom Number Generation: The C++ rand() function doesn't generate truly random numbers. It uses a 
+        deterministic algorithm that produces a sequence of numbers that appear random.
+    Seed Sets the Starting Point: srand() initializes the random number generator with a seed value, 
+        effectively telling it where in its sequence to start.
     
+    Without the srand(std::time(NULL)) line, the robotomy would have the exact same result (success or failure) every time for a given target, 
+    which defeats the purpose of having a 50% chance.
+    For a real-world application requiring better randomness, C++11 and later provide superior alternatives like <random> 
+    library with std::mt19937 and distribution objects, but for this educational example, srand() with rand() is sufficient.*/
+    
+    // Generate a random number and check if it's even or odd
+    // std::rand() generates a pseudo-random integer between 0 and RAND_MAX
+    // The modulo 2 operation (% 2) gives either 0 (even) or 1 (odd)
+    // In C++, non-zero values in conditionals are treated as true, zero as false
     if (std::rand() % 2)
-        std::cout << this->_target << " has been robotomized successfully!" << std::endl;
+       // If we got an odd number (1), the robotomy succeeds
+       std::cout << this->_target << " has been robotomized successfully!" << std::endl;
     else
-        std::cout << "Robotomy of " << this->_target << " failed." << std::endl;
+       // If we got an even number (0), the robotomy fails
+       std::cout << "Robotomy of " << this->_target << " failed." << std::endl;
 }
