1730ed70b954ed3f471abda46b3a2ff5c4201bd4
Peter Moelzer <moelzerpeter@gmail.com>
2025-06-12 15:04:35 +0200

    norm

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pmolzer <pmolzer@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 14:14:59 by pmolzer           #+#    #+#             */
-/*   Updated: 2025/05/13 10:38:32 by pmolzer          ###   ########.fr       */
+/*   Updated: 2025/06/12 15:03:29 by pmolzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "ShrubberyCreationForm.hpp"
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "Intern.hpp"
 #include <iostream>
 
 int main() 
@@ -31,33 +32,58 @@ int main()
         std::cout << low << std::endl;
         std::cout << "===========================" << std::endl << std::endl;
         
-        // Test ShrubberyCreationForm (sign: 145, exec: 137)
-        std::cout << "=== Testing ShrubberyCreationForm ===" << std::endl;
-        ShrubberyCreationForm shrub("garden");
-        low.signForm(shrub);
-        low.executeForm(shrub);
-        mid.signForm(shrub);        
-        mid.executeForm(shrub);
-        high.executeForm(shrub);
-        std::cout << "====================================" << std::endl << std::endl;
+        // Test Intern class
+        std::cout << "=== Testing Intern Class ===" << std::endl;
+        Intern someRandomIntern;
+        AForm* rrf;
+        AForm* scf;
+        AForm* ppf;
         
-        // Test RobotomyRequestForm (sign: 72, exec: 45)
-        std::cout << "=== Testing RobotomyRequestForm ===" << std::endl;
-        RobotomyRequestForm robot("\033[31mtarget\033[0m");
-        low.signForm(robot);
-        mid.signForm(robot);
-        mid.executeForm(robot);
-        high.signForm(robot);
-        high.executeForm(robot);
-        std::cout << "===================================" << std::endl << std::endl;
+        // Create forms using intern
+        rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+        scf = someRandomIntern.makeForm("shrubbery creation", "home");
+        ppf = someRandomIntern.makeForm("presidential pardon", "criminal");
         
-        // Test PresidentialPardonForm (sign: 25, exec: 5)
-        std::cout << "=== Testing PresidentialPardonForm ===" << std::endl;
-        PresidentialPardonForm pardon("\033[33mcriminal\033[0m");
-        mid.signForm(pardon);
-        high.signForm(pardon);
-        high.executeForm(pardon);
-        std::cout << "======================================" << std::endl << std::endl;
+        // Test invalid form name (this will return NULL and print error)
+        std::cout << "--- Testing invalid form ---" << std::endl;
+        AForm* invalidForm = someRandomIntern.makeForm("invalid form", "target");
+        if (!invalidForm)
+            std::cout << "Invalid form returned NULL as expected" << std::endl;
+        std::cout << "-----------------------------" << std::endl;
+        
+        std::cout << "============================" << std::endl << std::endl;
+        
+        // Test the created forms
+        if (rrf)
+        {
+            std::cout << "=== Testing Robotomy Request Form ===" << std::endl;
+            high.signForm(*rrf);
+            high.executeForm(*rrf);
+            std::cout << "=====================================" << std::endl << std::endl;
+        }
+        
+        if (scf)
+        {
+            std::cout << "=== Testing Shrubbery Creation Form ===" << std::endl;
+            mid.signForm(*scf);
+            mid.executeForm(*scf);
+            std::cout << "=======================================" << std::endl << std::endl;
+        }
+        
+        if (ppf)
+        {
+            std::cout << "=== Testing Presidential Pardon Form ===" << std::endl;
+            high.signForm(*ppf);
+            high.executeForm(*ppf);
+            std::cout << "========================================" << std::endl << std::endl;
+        }
+        
+        // Clean up dynamically allocated forms
+        delete rrf;
+        delete scf;
+        delete ppf;
+        // invalidForm is NULL, so no need to delete
+        
     } 
     catch (std::exception& e) 
     {
