fae1f63b543af41b2930737807ede7c1d307fd7b
Peter Moelzer <moelzerpeter@gmail.com>
2025-10-18 10:59:40 +0200

    adding new habits

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -10,27 +10,39 @@ public:
     // Orthodox Canonical Form
     MutantStack() : std::stack<T>() {}
     MutantStack(const MutantStack &other) : std::stack<T>(other) {}
-    MutantStack &operator=(const MutantStack &other) {
         std::stack<T>::operator=(other);
         return *this;
     }
     ~MutantStack() {}
 
-// Iterator typedefs (using the underlying container's iterators)
-    /* The purpose of the typedefs is to expose the iterator types from the underlying container and 
-    give them simple, standard names within the scope of MutantStack. std::stack
-  itself doesn't have an iterator type. We need to dig into its underlying container to find them.
-   * typename std::stack<T>::container_type: This part accesses the type of the underlying container (e.g., std::deque<T>).
-   * ::iterator: This then accesses the public iterator type defined within that container.
-  By creating these typedefs, we allow a user of MutantStack to write standard-looking code 
-  like MutantStack<int>::iterator it; without needing to know what the underlying
-  container is.*/
+    // Iterator typedefs - Exposing the underlying container's iterator types
+    //
+    // std::stack itself doesn't provide iterator types, but we can access them
+    // through its underlying container (typically std::deque<T>).
+    //
+    // Each typedef follows this pattern:
+    // 1. typename std::stack<T>::container_type - Access the underlying container type
+    // 2. ::iterator/const_iterator/etc - Access the specific iterator type from that container
+    // 3. typedef - Create an alias with a simple name in MutantStack's scope
+    //
+    // This allows users to write: MutantStack<int>::iterator it;
+    // Instead of: std::deque<int>::iterator it;
+    //
+    // The 'typename' keyword is required because we're accessing a type that depends
+    // on a template parameter T. Without it, the compiler wouldn't know that
+    // std::stack<T>::container_type::iterator refers to a type vs a value.
+    // Standard iterator - allows modification of elements during traversal
     typedef typename std::stack<T>::container_type::iterator iterator;
+    // Const iterator - read-only access to elements during traversal
     typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    // Reverse iterator - allows traversal from end to beginning (can modify elements)
     typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    // Const reverse iterator - read-only traversal from end to beginning
     typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-    // Functions to access iterators
+    // Iterator access functions
+    // Note: We use 'this->c' to access the underlying container
+    // 'c' is a protected member of std::stack that holds the actual container
     iterator begin() { return this->c.begin(); }
     iterator end() { return this->c.end(); }
     const_iterator begin() const { return this->c.begin(); }
