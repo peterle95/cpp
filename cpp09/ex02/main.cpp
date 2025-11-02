601f902c2551e0ab511222f3d9960db766af2a6c
Peter Moelzer <moelzerpeter@gmail.com>
2025-11-02 14:49:16 +0100

    sort check before algorithm

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,13 +1,36 @@
 #include "PmergeMe.hpp"
 
+bool isSorted(int argc, char **argv) 
+{
+    for (int i = 1; i < argc - 1; i++) 
+    {
+        int current = std::atoi(argv[i]);
+        int next = std::atoi(argv[i + 1]);
+        if (current > next)
+            return false;
+    }
+    return true;
+}
+
 int main(int argc, char **argv)
 {
 
-    // Should I imiplement functions for already sorted stacks?
     if (argc < 2)
     {
         std::cerr << "Usage: " << argv[0] << " <positive_integer_sequence>" << std::endl;
         return 1;
+    }
+
+        // Check if input is already sorted
+    if (isSorted(argc, argv)) 
+    {
+        std::cout << "Input sequence is already sorted!" << std::endl;
+        // Print the sequence
+        std::cout << "Sequence: ";
+        for (int i = 1; i < argc; i++)
+            std::cout << argv[i] << " ";
+        std::cout << std::endl;
+        return 0;
     }
 
     PmergeMe sorter;
